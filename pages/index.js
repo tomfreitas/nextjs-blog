1731455433a82fa107fd85ac7b7da4a9214aff8a
Wellington Freitas <wellington@shalomdigital.com.br>
2022-09-06 16:01:08 -0300

    ajuses

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,29 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts';
 
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData();
+    return {
+        props: {
+            allPostsData,
+        },
+    };
+}
 
-export default function Home() {
+export default function Home({ allPostsData }) {
+    console.log(allPostsData);
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>    
-      <section className={utilStyles.headingMd}>
-        <p>Descricao do site</p>
-        <p>
-            (This is a sample website - you’ll be building a site like this on{' '}
-            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-          </p>
-          <p>Acesse o blog <a href='/posts/first-post'>aqui</a></p>
+      {/* Keep the existing code here */}
+
+      {/* Add this <section> tag below the existing <section> tag */}   
+      <section className={ `${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLG}>Blog</h2>
+        <ul className={utilStyles.list}>
+            <li>{ allPostsData }</li>
+        </ul>
       </section>
     </Layout>
   );
